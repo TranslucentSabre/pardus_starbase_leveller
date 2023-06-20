3fe8e4eb9dbf1e8173c8841a44f70926c4074c15
Tim Myers <timothy.myers@adtran.com>
2023-06-20 09:56:46 -0500

    Modify when we create the observer and save base name

--- scripts/content.js
+++ scripts/content.js
@@ -2,21 +2,24 @@
 document.querySelector('#main').addEventListener('load', function (e) {
     // This observer lets us know when things change in the main frame so we can save the current food and water values for the base
     // we are at
-    const innerObserver = new MutationObserver( (mutataionList, observer ) => {
-        frame = document.querySelector("#main").contentWindow.document.querySelector("form[name=starbase_trade]");
-        // Only check values to save if we can find the "starbase_trade" form
-        if (frame) {
+    frameDoc = e.target.contentWindow.document.body;
+    baseForm = frameDoc.querySelector("form[name=starbase_trade]");
+    if (baseForm) {
+        //Only set up the observer if we are on the base page
+        const innerObserver = new MutationObserver( (mutataionList, observer ) => {
             // Rean from storage first so we only save when needed
             chrome.storage.local.get(["food","water"]).then((values) => {
-                var food = Number(frame.querySelector("#baserow1").children[2].textContent.replaceAll(",",""));
-                var water = Number(frame.querySelector("#baserow3").children[2].textContent.replaceAll(",",""));
+                var food = Number(baseForm.querySelector("#baserow1").children[2].textContent.replaceAll(",",""));
+                var water = Number(baseForm.querySelector("#baserow3").children[2].textContent.replaceAll(",",""));
                 if ((food && water ) && ( food != values.food || water != values.water)) {
-                    chrome.storage.local.set({"food":food, "water":water}).then(() => {
-                       console.log("Saved - Food: "+food+", Water: "+water);
+                    //Need to go up one layer so that the right a tag is selected
+                    base=frameDoc.querySelector("a").textContent;
+                    chrome.storage.local.set({"food":food, "water":water, "base":base}).then(() => {
+                       console.log("Saved - Food: "+food+", Water: "+water+", Base: "+base);
                     });
                 }
             });
-        }
-    });
-    innerObserver.observe(e.target.contentWindow.document.body, { subtree: true, attributes: true});
+        });
+        innerObserver.observe(frameDoc, { subtree: true, attributes: true});
+    }
 })
