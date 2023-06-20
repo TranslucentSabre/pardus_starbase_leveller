de0e41a3a21bc6a768bd4988ee521fd64dadb0cf
Timothy Myers <temyers240@gmail.com>
2023-06-19 22:05:26 -0500

    Example of how to use the storage api to keep state

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,9 @@
-console.log("Test on pardus pages")
+var loads = 0;
+chrome.storage.local.get(["loads"]).then((result) => {
+    if (result.loads) {
+        loads = result.loads;
+    }
+    loads++;
+    console.log("Test on pardus pages: ", loads);
+    chrome.storage.local.set({"loads":loads})
+})
