888547f88e58b0f7ff8c54b43c7b0aed76bd0b6b
Timothy Myers <temyers240@gmail.com>
2023-06-19 22:20:24 -0500

    Added query to get food and water, but I still don't know how to trigger this properly.

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,12 @@
-var loads = 0;
-chrome.storage.local.get(["loads"]).then((result) => {
+var food = Number(document.querySelector("#baserow1").children[2].textContent);
+if (food) {
+    chrome.storage.local.set({"food":food});
+}
+var water = Number(document.querySelector("#baserow3").children[2].textContent);
+if (water) {
+    chrome.storage.local.set({"water":water});
+}
+chrome.storage.local.get(["food","water"]).then((result) => {
     if (result.loads) {
         loads = result.loads;
     }
